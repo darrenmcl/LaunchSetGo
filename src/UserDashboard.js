5a15b1d6f8eb4e57b4b499d090f8e01bbdd3fe73
Darren McLaughlin <darren@getperformancemarketing.com>
2023-10-19 10:52:18 -0400

    Removed config

--- src/UserDashboard.js
+++ src/UserDashboard.js
@@ -3,18 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import './UserDashboard.css';
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyB9fzqzLlKy7m8qhk_qQa9RIYid9nPQ6h4",
-    authDomain: "launch-set-go.firebaseapp.com",
-    projectId: "launch-set-go",
-    storageBucket: "launch-set-go.appspot.com",
-    messagingSenderId: "408122065538",
-    appId: "1:408122065538:web:c0ca13a5d944f08b33ea9b",
-    measurementId: "G-J9615VMT93"
-  };
-  
+import { firebaseConfig } from './firebaseConfig.js';
   
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB9fzqzLlKy7m8qhk_qQa9RIYid9nPQ6h4",
+    authDomain: "launch-set-go.firebaseapp.com",
+    projectId: "launch-set-go",
+    storageBucket: "launch-set-go.appspot.com",
+    messagingSenderId: "408122065538",
+    appId: "1:408122065538:web:c0ca13a5d944f08b33ea9b",
+    measurementId: "G-J9615VMT93"
+  };
