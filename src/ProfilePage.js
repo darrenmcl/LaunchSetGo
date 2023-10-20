de9aa7c34b6ff6ec9791ed88c63e02c33b00ac51
Darren McLaughlin <darren@getperformancemarketing.com>
2023-10-20 09:30:02 -0400

    Updated.

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,46 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { firebaseConfig } from './firebaseConfig.js';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import './ProfilePage.css';
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);  // Make sure you're not initializing this multiple times across files
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 function ProfilePage() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setEmail(user.email);
+      } else {
+        // Handle user not signed in
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    createUserWithEmailAndPassword(auth, email, 'password')
-      .then((userCredential) => {
-        const user = userCredential.user;
-        db.collection('users').doc(user.uid).set({
-          name,
-          email,
-        })
-        .then(() => {
-          setIsSubmitted(true);
-        })
-        .catch((error) => {
-          console.error('Error adding user to database:', error);
-        });
-      })
-      .catch((error) => {
-        console.error('Error creating user:', error);
-      });
+    try {
+      const userDoc = doc(db, 'users', auth.currentUser.uid);
+      await setDoc(userDoc, { name, email });
+      setIsSubmitted(true);
+    } catch (error) {
+      console.error('Error adding user to database:', error);
+      setErrorMessage(error.message);
+    }
   };
 
   return (
@@ -50,15 +51,15 @@ function ProfilePage() {
           Name:
           <input type="text" value={name} onChange={handleNameChange} />
         </label>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={handleEmailChange} />
-        </label>
         <button type="submit">Submit</button>
       </form>
-      {isSubmitted && <p>User created successfully!</p>}
+      {isSubmitted && <p>User updated successfully!</p>}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
   );
 }
 
 export default ProfilePage;
+
+
+
--- src/UserDashboard.js
+++ src/UserDashboard.js
@@ -5,47 +5,52 @@ import { initializeApp } from 'firebase/app';
 import './UserDashboard.css';
 import { firebaseConfig } from './firebaseConfig.js';
   
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
   
-  const UserDashboard = () => {
-    const [user, setUser] = useState(null);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged((user) => {
-        if (user) {
-          setUser(user);
-        } else {
-          navigate('/', { replace: true });
-        }
-      });
-  
-      // Cleanup subscription on unmount
-      return () => unsubscribe();
-    }, [navigate]);
-  
-    const handleLogout = async () => {
-      try {
-        await auth.signOut();
-        console.log('Logged out successfully!');
-      } catch (error) {
-        console.error(error);
+const UserDashboard = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        navigate('/', { replace: true });
       }
-    };
-  
-    return (
-      <div>
-        {user ? (
-          <div>
-            <h1>Welcome, {user.email}!</h1>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-        ) : (
-          <h1>Please log in to view your dashboard.</h1>
-        )}
-      </div>
-    );
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [navigate]);
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      console.log('Logged out successfully!');
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  const goToProfile = () => {
+    navigate('/profile');
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h1>Welcome, {user.email}!</h1>
+          <button onClick={handleLogout} className="goToProfileButton">Logout</button>
+          <button onClick={goToProfile} className="goToProfileButton">Go to Profile</button>
+        </div>
+      ) : (
+        <h1>Please log in to view your dashboard.</h1>
+      )}
+    </div>
+  );
+};
   
-  export default UserDashboard;
+export default UserDashboard;
